2d840f290fc3b6b0b27dc50ef1e39d2387ea339b
Ryan Kruberg <rkruberg@gmail.com>
2023-10-03 06:13:32 +0300

    change: type info

--- frontend/types/image.d.ts
+++ frontend/types/image.d.ts
@@ -1,5 +1,12 @@
 export interface Image {
+  /**
+   * ID in the database
+   */
   id: number;
+  /**
+   * Url of the image
+   * @example "https://picsum.photos/id/488/1772/1181"
+   */
   imageUrl: string;
   /**
    * The original image width -- **MAX** image width
