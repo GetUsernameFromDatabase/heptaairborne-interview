f80a5e10d393a97b2d9d328af9b80d13899f2720
Ryan Kruberg <rkruberg@gmail.com>
2023-10-08 20:16:13 +0300

    refactor(backend): use image service

--- backend/src/main/java/com/hainterview/backend/controller/ImageController.java
+++ backend/src/main/java/com/hainterview/backend/controller/ImageController.java
@@ -1,34 +1,30 @@
 package com.hainterview.backend.controller;
 
+import com.hainterview.backend.entity.ImageEntity;
+import com.hainterview.backend.service.ImageService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.hainterview.backend.entity.ImageEntity;
-import com.hainterview.backend.repository.ImageRepository;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/images")
 public class ImageController {
 
-    private final ImageRepository imageRepository;
+  private final ImageService imageService;
 
-    @Autowired
-    public ImageController(ImageRepository imageRepository) {
-        this.imageRepository = imageRepository;
-    }
+  @Autowired
+  public ImageController(ImageService imageService) {
+    this.imageService = imageService;
+  }
 
-    @GetMapping
-    public Page<ImageEntity> getAllImages(
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size
-    ) {
-        Pageable pageable = PageRequest.of(page, size);
-        return imageRepository.findAll(pageable);
-    }
+  @GetMapping
+  public Page<ImageEntity> getAllImages(
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size) {
+    return imageService.getAllImages(page, size);
+  }
 }
---
+++ backend/src/main/java/com/hainterview/backend/service/ImageService.java
@@ -0,0 +1,25 @@
+package com.hainterview.backend.service;
+
+import com.hainterview.backend.entity.ImageEntity;
+import com.hainterview.backend.repository.ImageRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+  private final ImageRepository imageRepository;
+
+  @Autowired
+  public ImageService(ImageRepository imageRepository) {
+    this.imageRepository = imageRepository;
+  }
+
+  public Page<ImageEntity> getAllImages(int page, int size) {
+    Pageable pageable = Pageable.ofSize(size).withPage(page);
+    return imageRepository.findAll(pageable);
+  }
+}
