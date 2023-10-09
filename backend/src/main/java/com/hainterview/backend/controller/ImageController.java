a4a402977e8d1fb82a7df6ab7714bdcabaf7d06a
Ryan Kruberg <rkruberg@gmail.com>
2023-10-09 09:41:11 +0300

    feat: controller error handleing

--- backend/src/main/java/com/hainterview/backend/controller/ImageController.java
+++ backend/src/main/java/com/hainterview/backend/controller/ImageController.java
@@ -5,26 +5,34 @@ import com.hainterview.backend.service.ImageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/images")
 public class ImageController {
 
-  private final ImageService imageService;
+    private final ImageService imageService;
 
-  @Autowired
-  public ImageController(ImageService imageService) {
-    this.imageService = imageService;
-  }
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
-  @GetMapping
-  public Page<ImageEntity> getAllImages(
-      @RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "10") int size) {
-    return imageService.getAllImages(page, size);
-  }
+    @GetMapping
+    public ResponseEntity<Page<ImageEntity>> getAllImages(
+            @RequestParam(defaultValue = "0") @Min(value = 0, message = "Page number must be greater than or equal to 0") int page,
+            @RequestParam(defaultValue = "10") @Min(value = 1, message = "Page size must be greater than or equal to 1") int size) {
+        Page<ImageEntity> images = imageService.getAllImages(page, size);
+        return new ResponseEntity<>(images, HttpStatus.OK);
+    }
+
+    // TODO: make a system that handles exceptions for all controllers
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        // Handle the exception and return an appropriate response
+        return new ResponseEntity<>("An error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
